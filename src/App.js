fb9f48aad15d605de81029c39a8d49410dcb2fcb
OtavioFilipe <darksnack16@gmail.com>
2022-07-01 00:14:04 -0300

    studing hooks :rocket:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.github.com/users/OtavioFilipe/repos');
+      const data = await response.json();
+      setRepositories(data);
+    })();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
+  ); 
 }
-
-export default App;
